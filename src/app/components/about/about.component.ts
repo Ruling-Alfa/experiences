70f7056caae04aab34d0bc225bfdad97c0c4c605
neel.acharya@live.com <neel.acharya@live.com>
2022-10-07 23:26:25 -0400

    lazy load of heavy css bundle

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit(): void {
+    this.loadStyle('about-component');
+  }
+
+  private loadStyle(styleName: string) {
+    let aboutPageTheme = this.document.getElementById(
+      'about-page-theme'
+    ) as HTMLLinkElement;
+    if (aboutPageTheme) {
+      aboutPageTheme.href = `${styleName}.css`;
+    }
   }
 
 }
