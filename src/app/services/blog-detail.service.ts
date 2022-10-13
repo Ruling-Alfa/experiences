5a9c3e0ec9fff0374472cb7647745de8887f8f05
neel.acharya@live.com <neel.acharya@live.com>
2022-10-12 23:43:29 -0400

    Added contents about Open/Close - Blog 1

--- src/app/services/blog-detail.service.ts
+++ src/app/services/blog-detail.service.ts
@@ -9,7 +9,7 @@ export class BlogDetailService {
   constructor(private http: HttpClient) { }
 
   GetBlogDetail(id: number): string{
-    const detail:string = ` <div class="jumbotron">
+    const detail:string = `<div class="jumbotron">
     <h1 class="display-4">SOLID Principles</h1>
     <p class="lead">A detailed guide to doing solid principles in code</p>
     <hr class="my-4">
@@ -47,75 +47,199 @@ export class BlogDetailService {
         </p>
         <pre class="pre-scrollable border rounded bg-light" ng-non-bindable>
             <code ng-non-bindable>
-public class Employee
-&#123;
-    public int EmployeeId &#123; get; set; &#125;
-    public string? Name &#123; get; set; &#125;
-    public string? Email &#123; get; set; &#125;
-    public double Salary &#123; get; set; &#125;
-    public double CalculateSalary(int hours)
+    public class Employee
     &#123;
-        return this.Salary * hours;
-    &#125;
-
-    public bool ValidateEmail()
-    &#123;
-        var isValid = false;
-        if (this.Email is not null)
+        public int EmployeeId &#123; get; set; &#125;
+        public string? Name &#123; get; set; &#125;
+        public string? Email &#123; get; set; &#125;
+        public double Salary &#123; get; set; &#125;
+        public double CalculateSalary(int hours)
         &#123;
-            var emailRegEx = new Regex(@&#x22;^([\w -] + (?:\.[\w-]+)*)@((?:[\w -] +\.)*\w[\w -]&#123; 0,66&#125;)\.([a - z]&#123; 2,6&#125; (?:\.[a-z]&#123; 2&#125;)?)$/ i&#x22;);
-            isValid = emailRegEx.IsMatch(this.Email);
+            return this.Salary * hours;
         &#125;
-        return isValid;
+    
+        public bool ValidateEmail()
+        &#123;
+            var isValid = false;
+            if (this.Email is not null)
+            &#123;
+                var emailRegEx = new Regex(@&#x22;^([\w -] + (?:\.[\w-]+)*)@((?:[\w -] +\.)*\w[\w -]&#123; 0,66&#125;)\.([a - z]&#123; 2,6&#125; (?:\.[a-z]&#123; 2&#125;)?)$/ i&#x22;);
+                isValid = emailRegEx.IsMatch(this.Email);
+            &#125;
+            return isValid;
+        &#125;
+    
+    
     &#125;
-
-
-&#125;
             </code>
         </pre>
 
-        <p>This may look good at first glance, but if we try to understand the need of <var>ValidateEmail</var> in the class, it becomes pretty clear that this is an extra behaviour and shall be moved out of the class into some other class. Moving this part out
-            of the <var>Employee</var> class also makes it more reusable in other parts of the project wherever email and email validation is required.
+        <p>This may look good at first glance, but if we try to understand the need of Validate Email in the class, it becomes pretty clear that this is an extra behaviour and shall be moved out of the class into some other class. Moving this part out
+            of the employee class also makes it more reusable in other parts of the project wherever email and email validation is required.
         </p>
         <p>The improved code looks like the one below</p>
 
         <pre class="pre-scrollable border rounded bg-light" ng-non-bindable>
             <code ng-non-bindable>
-public class Employee
-&#123;
-    public int EmployeeId &#123; get; set; &#125;
-    public string? Name &#123; get; set; &#125;
-    public Email? Email &#123; get; set; &#125;
-    public double Salary &#123; get; set; &#125;
-    public double CalculateSalary(int hours)
+    public class Employee
     &#123;
-        return this.Salary * hours;
-    &#125;
-&#125;
-
-public class Email
-&#123;
-    public string? EmailId &#123; get; set; &#125;
-
-    public bool ValidateEmail()
-    &#123;
-        var isValid = false;
-        if (this.EmailId is not null)
+        public int EmployeeId &#123; get; set; &#125;
+        public string? Name &#123; get; set; &#125;
+        public Email? Email &#123; get; set; &#125;
+        public double Salary &#123; get; set; &#125;
+        public double CalculateSalary(int hours)
         &#123;
-            var emailRegEx = new Regex(@"^([\w -] + (?:\.[\w-]+)*)@((?:[\w -] +\.)*\w[\w -]&#123; 0,66&#125;)\.([a - z]&#123; 2,6&#125; (?:\.[a-z]&#123; 2&#125;)?)$/ i");
-            isValid = emailRegEx.IsMatch(this.EmailId);
+            return this.Salary * hours;
         &#125;
-        return isValid;
     &#125;
-&#125;
+
+    public class Email
+    &#123;
+        public string? EmailId &#123; get; set; &#125;
+
+        public bool ValidateEmail()
+        &#123;
+            var isValid = false;
+            if (this.EmailId is not null)
+            &#123;
+                var emailRegEx = new Regex(@"^([\w -] + (?:\.[\w-]+)*)@((?:[\w -] +\.)*\w[\w -]&#123; 0,66&#125;)\.([a - z]&#123; 2,6&#125; (?:\.[a-z]&#123; 2&#125;)?)$/ i");
+                isValid = emailRegEx.IsMatch(this.EmailId);
+            &#125;
+            return isValid;
+        &#125;
+    &#125;
             </code>
         </pre>
     </div>
     <!-- <p class="lead">
         <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-    </p> -->
+    </p>  -->
 
-    To be continued...
+    <p>Some people also believe that Single responsibility and open/close principle are the same things</p>
+
+
+    <div id="openClose">
+        <h3>
+            <strong>O</strong>pen/Close principle
+        </h3>
+        <p>A class once implemented, shall be open for extension or for addition of further more features but shall be closed for modification in the implementation. Meaning, any logic shall not be changes to accomodate more features. Of-course bug fixes
+            and refactoring is allowed.
+        </p>
+
+        <p>For instance lets say that we have a class for <var>Notifications</var> and it contains logic for sending email every time salary for an employee is deposited to their account.
+        </p>
+        <pre class="pre-scrollable border rounded bg-light" ng-non-bindable>
+            <code ng-non-bindable>
+        public class Notifications
+        &#123;
+            public string? EmailId &#123; get; set; &#125;
+        
+            public void sendEmail()
+            &#123;
+                //some logic to send email
+            &#125;
+        &#125;
+            </code>
+        </pre>
+
+        <p>Now lets say that the requirements were refined and a new feature was introduced where we also send an SMS on employee phone. Lets add the same code to our class. The first thing that comes to mind is to add an if condition and get it done.
+        </p>
+
+        <pre class="pre-scrollable border rounded bg-light" ng-non-bindable>
+            <code ng-non-bindable>
+        public class Notifications
+        &#123;
+            public string? EmailId &#123; get; set; &#125;
+            
+            public string? Mobile &#123; get; set; &#125;
+        
+            public void NotifyUser()
+            &#123;
+                if (this.EmailId is not null)
+                    this.SendEmail();
+                if (this.Mobile is not null)
+                    this.SendSMS();
+            &#125;
+            
+            private void SendEmail()
+            &#123;
+                //some logic to send email
+            &#125;
+            
+            private void SendSMS()
+            &#123;
+                //some logic to send sms
+            &#125;
+        &#125;
+            </code>
+        </pre>
+
+        <p>Further more notifications via push notifications on mobile app is added. We again have to go and modify the class <var>Notifications</var>. This means that we are constantly modifying the class to add new and new features. A better approach
+            to this would be to move this all out of the <var>Notifications</var> class and just have the trigger there to fire the notifications. For this we will use inheritance and polymorphism.
+        </p>
+
+        <pre class="pre-scrollable border rounded bg-light" ng-non-bindable>
+            <code ng-non-bindable>
+        public class Notifications
+        &#123;
+            public void NotifyUser(INotificationTypes notificationTypes)
+            &#123;
+                notificationTypes.SendNotification();
+            &#125;
+        &#125;
+        
+        public interface INotificationTypes
+        &#123;
+            public void SendNotification();
+        &#125;
+
+        public class Email : INotificationTypes
+        &#123;
+            public string? EmailId &#123; get; set; &#125;
+        
+            public void SendNotification()
+            &#123;
+                if (this.EmailId is not null)
+                &#123;
+                    //some logic to send email
+                &#125;
+            &#125;
+        &#125;
+        
+        public class SMS : INotificationTypes
+        &#123;
+            public string? Mobile &#123; get; set; &#125;
+        
+            public void SendNotification()
+            &#123;
+                if (this.Mobile is not null)
+                &#123;
+                    //some logic to send sms
+                &#125;
+            &#125;
+        
+        &#125;
+        
+        public class PushNotification : INotificationTypes
+        &#123;
+            public string? DeviceId &#123; get; set; &#125;
+        
+            public void SendNotification()
+            &#123;
+                if (this.DeviceId is not null)
+                &#123;
+                    //some logic to send push notification to mobile devices
+                &#125;
+            &#125;
+        
+        &#125;
+            </code>
+        </pre>
+        <p>Here we can easily add more and more types of notifications by inheriting the <var>INotificationTypes</var> interface and trigger them using the <var>Notifications</var> class object. It becomes pretty clear by using the inheritance and polymorphism
+            that the instance of class implementing interface will be available to the <var>Notification</var> class to send notifications, without even considering the actual implementation. This lets us create infinitely many classes and implementations
+            for adding many new notification types to be handled.</p>
+    </div>
+    To be Continued ...
 </div>`;
 
     return detail;
